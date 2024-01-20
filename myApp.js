d6c1facc1444c3d3147b2928a3ba50bec156db7d
Shane Pearson <bubbafoley@gmail.com>
2024-01-20 02:15:33 +0000

    Basic Node and Express part 1 - 6.

--- myApp.js
+++ myApp.js
@@ -1,12 +1,21 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
 
+app.get("/json", (req, res) => {
+    let msg = "Hello json";
+    res.json({"message": (process.env.MESSAGE_STYLE == "uppercase" ? msg.toUpperCase() : msg)});
+});
 
-
-
-
+console.log("Hello World");
 
 
 
